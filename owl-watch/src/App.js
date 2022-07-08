f77571e5b7f183d4b84c0c79d71fb88c7724affa
michaelngo21 <michaelngomn21@gmail.com>
2022-07-07 20:39:02 -0500

    testing navigation bar

--- owl-watch/src/App.js
+++ owl-watch/src/App.js
@@ -1,10 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages';
+import Clock from './pages/clock';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route path='/clock' element={<Clock/>} />
+          </Routes>
+        </Router>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. *hello from nat :)*
@@ -18,6 +31,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <a href="log-in">Log In</a>
+      </div>
     </div>
   );
 }
