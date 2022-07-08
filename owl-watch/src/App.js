6380b2e2b5b0d31683269bac6883dcf76da0af56
michaelngo21 <michaelngomn21@gmail.com>
2022-07-07 20:46:59 -0500

    testing navbar feature

--- owl-watch/src/App.js
+++ owl-watch/src/App.js
@@ -9,32 +9,13 @@ import Clock from './pages/clock';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route exact path='/' element={<Home />} />
-            <Route path='/clock' element={<Clock/>} />
-          </Routes>
-        </Router>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. *hello from nat :)*
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div>
-        <a href="log-in">Log In</a>
-      </div>
-    </div>
+    <Router>
+    <Navbar />
+    <Routes>
+      <Route exact path='/' element={<Home />} />
+      <Route path='/clock' element={<Clock/>} />
+    </Routes>
+    </Router>
   );
 }
 
---
+++ owl-watch/src/pages/clock.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Clock = () => {
+    return (
+        <div>
+            <h1>
+                This is my page for the clock feature! :D
+            </h1>
+        </div>
+    );
+};
+
+export default Clock;
