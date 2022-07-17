0bde849cb29992e85f75ab68a79ec5c7f315bf80
Tom <tomvincent02@icloud.com>
2022-07-17 12:23:26 -0500

    Created a DB using MongoDB Atlas and made a simple CRUD operations

--- owl-watch/backend/server.js
+++ owl-watch/backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
@@ -8,6 +9,22 @@ const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri,{
+    }).then(()=>{
+     console.log('database connected')
+}).catch(err=>{
+    console.log('database not connected',err)
+});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
+const usersRouter = require('./routes/users');
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
     console.log(`Express Server is running on port: ${port}`);
