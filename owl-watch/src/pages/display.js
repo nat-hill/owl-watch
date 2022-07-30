aa385cb870215d091915208ea83c84947998e32f
michaelngo21 <michaelngomn21@gmail.com>
2022-07-30 12:54:04 -0500

    unfinished version of display: attempting to fetch data from API.

--- owl-watch/src/pages/display.js
+++ owl-watch/src/pages/display.js
@@ -1,32 +1,66 @@
 import React from 'react';
 import { PieChart, Pie, Sector, Cell, ResponsiveContainer, Tooltip, Legend } from 'recharts';
+import { useState, useEffect } from "react";
   
-  
+export function fetchTest(){
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(null);
+    const [error, setError] = useState(null);
+    useEffect(() => {
+        const getData = async () => {
+          try {
+            const response = await axios.get(
+              `https://localhost:3002/users/62dd8efe465554e17bd84158`
+            );
+            setData(response.data);
+            setError(null);
+          } catch (err) {
+            setError(err.message);
+            setData(null);
+          } finally {
+            setLoading(false);
+          }
+        };
+        getData();
+      }, []);
+      
+      return (
+        <div>
+            { data }
+        </div>
+      )
+
+}
+
 const display = () => {
   
-// Sample data
-const data = [
-  {name: 'Geeksforgeeks', secondsSpent: 400, fill: "green"},
-  {name: 'Technical scripter', secondsSpent: 700, fill: "red"},
-  {name: 'Geek-i-knack', secondsSpent: 200, fill: "blue"},
-  {name: 'Geek-o-mania', secondsSpent: 1000, fill: "purple"}
-];  
+    // Sample data
+    const data = [
+    {name: 'Geeksforgeeks', secondsSpent: 400, fill: "green"},
+    {name: 'Technical scripter', secondsSpent: 700, fill: "red"},
+    {name: 'Geek-i-knack', secondsSpent: 200, fill: "blue"},
+    {name: 'Geek-o-mania', secondsSpent: 1000, fill: "purple"}
+    ];  
 
-return (
-    <div>
-        <h1>
-            Display
-        </h1>
-        
-        <PieChart width={700} height={700}>
-            <Pie data={data} 
-            dataKey={"secondsSpent"} 
-            nameKey={"name"} 
-            outerRadius={300}/>
-            <Legend />
-        </PieChart>
-    </div>
-);
+    const testData = fetchTest();
+
+    return (
+        <div>
+            <h1>
+                Display
+            </h1>
+            
+            <PieChart width={700} height={700}>
+                <Pie data={data} 
+                dataKey={"secondsSpent"} 
+                nameKey={"name"} 
+                outerRadius={300}/>
+                <Legend />
+            </PieChart>
+
+            { testData }
+        </div>
+    );
 }
 export default display;
 
